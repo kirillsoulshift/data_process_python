f6ed35a984d9a849907e42d81ac267f63db6db0c
kirillsoulshift <108855752+kirillsoulshift@users.noreply.github.com>
2022-07-07 15:22:58 +0700

    Update test2.py

--- process/test2.py
+++ process/test2.py
@@ -1,1 +1,5 @@
-
+import webbrowser
+webbrowser.open('https://github.com/kirillsoulshift/data_process_python', new=1) # window
+webbrowser.open('http://', new=2) # tab
+webbrowser.open('http://', new=2) # tab
+webbrowser.open('http://', new=2) # tab
