ba69696797514b22f7992d45f6ac0b578303fcde
kirillsoulshift <108855752+kirillsoulshift@users.noreply.github.com>
2023-06-08 17:19:18 +0700

    Delete start_https.py
    obsolete script deleted

--- process/start_https.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-webbrowser.open('https://github.com/kirillsoulshift/data_process_python', new=1) # window
-webbrowser.open('http://', new=2) # tab
-webbrowser.open('http://', new=2) # tab
-webbrowser.open('http://', new=2) # tab
